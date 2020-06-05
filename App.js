b55e7727d3157a5228eac9495fbb47391cdc071a
umaib <youmaybal@gmail.com>
2020-06-05 16:47:24 -0400

    initial commit

--- App.js
+++ App.js
@@ -1,19 +1,92 @@
+//imports
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, Text, TextInput, Button, TouchableOpacity} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+//create
+class App extends React.Component{
+  state = {
+    text: "",
+    todo: []
+  }
+  addTodo = () => {
+    // this.setState({todo: this.state.text})
+    var newTodo = this.state.text;
+    var arr = this.state.todo;
+    arr.push(newTodo);
+    this.setState({todo: arr, text: ""});
+  }
+  deleteTodo = (t) => {
+    var arr = this.state.todo;
+    var pos = arr.indexOf(t);
+    arr.splice(pos,1);
+    this.setState({todo: arr});
+  }
+  renderTodos = () => {
+    return this.state.todo.map(t=>{
+      return (
+        <TouchableOpacity key={t}>
+        <Text 
+          style={styles.todo}
+          key={t}
+          onPress={()=>{this.deleteTodo(t)}}
+        >{t}</Text>
+        </TouchableOpacity>
+      )
+    })
+  }
+  render(){
+    return(
+      <View style={styles.wholeStyle}>
+      <View style={styles.viewStyle}>
+        <Text style={styles.header}>Checklist App :)</Text>
+        <TextInput 
+          style={styles.inputStyle}
+          onChangeText={(text)=>this.setState({text})}
+          value={this.state.text}
+        />
+        <Button
+          title="Add ToDo"
+          color="#ee0290"
+          onPress={this.addTodo}
+          />
+        <View style={{marginTop: 100}}/>
+        {this.renderTodos()}
+        {/* <Button
+        title="Remove from list"
+        /> */}
+      </View>
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles = {
+  wholeStyle: {
+    backgroundColor: "#F2FDE4",
+    flex:1
   },
-});
+  viewStyle: {
+    marginTop: 50, 
+    alignItems: 'center', 
+    justifyContent: 'center',
+    margin: 10
+  },
+  inputStyle: {
+    height: 40,
+    width: "90%",
+    borderColor: "#ee0290",
+    borderWidth: 1
+  },
+  header:{
+    fontSize: 30,
+    color: "#ee0290",
+    fontWeight: "bold"
+  },
+  todo:{
+    fontSize: 22,
+    color: "gray"
+  }
+}
+
+//exports
+export default App;
---
+++ README.txt
@@ -0,0 +1,19 @@
+Author: Umai Balendra
+Date: June 4, 2020
+Project: React Native Expo ToDo App
+
+Sources: 
+- React Native Expo YouTube videos 
+(https://www.youtube.com/watch?v=HX0R_yB00kc&list=PLlmqcqVGwKLb0GNRU7ZtibnDyVcno-Vs4&index=9)
+
+Installation and Run Instructions:
+- You will need expo-cli to run this app (if you already have it installed, skip this step):
+'npm install expo-cli --global'
+
+- You can start the app with:
+'expo start'
+
+- Open an Android/iOS emulator via AndroidStudio or XCode
+
+- Type 'a' to run the app on an Android emulator, or 'i' to run the
+app on an iOS simulator
